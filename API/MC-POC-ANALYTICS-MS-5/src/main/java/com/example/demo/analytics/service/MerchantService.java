6625a737ba02a1c14e7a08080b5c366a453aa659
Kishor Baliram Kshirsagar (Digital) <KI20018806@wipro.com>
2018-11-28 20:25:25 +0530

    small corrections

--- API/MC-POC-ANALYTICS-MS-5/src/main/java/com/example/demo/analytics/service/MerchantService.java
+++ API/MC-POC-ANALYTICS-MS-5/src/main/java/com/example/demo/analytics/service/MerchantService.java
@@ -1,7 +1,10 @@
 package com.example.demo.analytics.service;
 
+import org.springframework.stereotype.Service;
+
 import com.example.demo.analytics.model.Merctranscategory;
 
+@Service("merchantService")
 public interface MerchantService {
 	
 	public Iterable<Merctranscategory> getAvailableMerchants();
